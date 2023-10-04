c2fea78f88603e15ebbe77c6ef61130acf444a99
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2023-10-04 15:06:06 -0700

    add some modifications

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_subtree, r_subtree;
 
-	if (!tree || (!(tree->left) && !(tree->right)))
+	if (!tree)
 		return (0);
 	l_subtree = binary_tree_height(tree->left);
 	r_subtree = binary_tree_height(tree->right);
